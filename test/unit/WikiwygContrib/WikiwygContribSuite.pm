30f9baaa73c77d88db310335beaa76062c5eb6e2
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/WikiwygContrib@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/WikiwygContrib/WikiwygContribSuite.pm
+++ test/unit/WikiwygContrib/WikiwygContribSuite.pm
@@ -3,8 +3,8 @@ package WikiwygContribSuite;
 use Unit::TestSuite;
 our @ISA = qw( Unit::TestSuite );
 
-sub name { 'WikiwygContribSuite' };
+sub name { 'WikiwygContribSuite' }
 
-sub include_tests { qw(WikiwygContribTests) };
+sub include_tests { qw(WikiwygContribTests) }
 
 1;
